787682fc67f20459d9d3f4ac3b26630007cad3ce
liuhao <2537945384@qq.com>
2020-03-07 15:08:47 +0800

    mutex

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <string.h>
 
 static int result = 0;
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t cnt = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t cnt_empty = PTHREAD_COND_INITIALIZER;
 void *start(void *str){
     int s;
     printf("hello form subthread %s \n", (char *) str);
@@ -19,10 +22,31 @@ void *start(void *str){
 void *addNum(void *loopNum){
     long loop = (long) loopNum;
     for (int i = 0; i < loop; ++i) {
+        pthread_mutex_lock(&mtx);//3. lock
+        while (result >= 3) {
+            pthread_cond_wait(&cnt_empty, &mtx);
+        }
         result++;
+        printf("produce %d\n",result);
+        pthread_mutex_unlock(&mtx);//4. unlock
+        pthread_cond_signal(&cnt);
     }
     return nullptr;
 }
+
+void *consume(void *loopNum){
+    long loop = (long) loopNum;
+    for (int i = 0; i < 2*loop; ++i) {
+        pthread_mutex_lock(&mtx);
+        while (result <= 0) {//注意这里需要用whie循环而不是if
+            pthread_cond_wait(&cnt,&mtx);
+        }
+        printf("consume result %d\n", --result);
+        pthread_mutex_unlock(&mtx);
+        pthread_cond_signal(&cnt_empty);
+    }
+    return nullptr;
+};
 
 int main() {
     pthread_t pt;
@@ -39,9 +63,9 @@ int main() {
         printf("pthread join get return value %ld\n", (long)len);
     }
 
-    pthread_t addPt1, addPt2;
+    pthread_t addPt1, addPt2,consumePt;
 
-    long loopNum = 100;
+    long loopNum = 10;
     s = pthread_create(&addPt1, nullptr, addNum, (void *)loopNum);
     if (s == 0) {
         printf("create addPt1 success %d \n", s);
@@ -56,6 +80,13 @@ int main() {
         printf("create addPt2 fail %d \n", s);
     }
 
+    s = pthread_create(&consumePt, nullptr, consume, (void *)loopNum);
+    if (s == 0) {
+        printf("create consumePt success %d \n", s);
+    } else {
+        printf("create consumePt fail %d \n", s);
+    }
+
     s=pthread_join(addPt1, nullptr);
     if (s == 0) {
         printf("addPt1 terminal success %d \n", s);
@@ -67,6 +98,13 @@ int main() {
         printf("addPt2 terminal success %d \n", s);
     } else {
         printf("addPt2 terminal fail %d \n", s);
+    }
+
+    s=pthread_join(consumePt, nullptr);
+    if (s == 0) {
+        printf("consumePt terminal success %d \n", s);
+    } else {
+        printf("consumePt terminal fail %d \n", s);
     }
 
     printf("after add the result is %d", result);
